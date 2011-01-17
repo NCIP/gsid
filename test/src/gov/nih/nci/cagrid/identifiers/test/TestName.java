0ed59e12acd475eadcbce2ee869c5b58ff6fb7e7
swayamps <swayamps>
2011-01-17 14:26:14 +0000

    updated the Stress Test Cases for Service.
    SVN-Revision: 29

---
+++ test/src/gov/nih/nci/cagrid/identifiers/test/TestName.java
@@ -0,0 +1,13 @@
+package gov.nih.nci.cagrid.identifiers.test;
+
+public enum TestName
+{
+	REGISTER_GSID,
+	PARENT_HIERARCHY,
+	CHILD_HIERARCHY,
+	VALIDATE_IDENTIFIER,
+	RESOLVE_IDENTIFIER,
+	ADD_SITE,
+	BATCH_IDENTIFIERS,
+	ALL
+}
